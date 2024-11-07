63c70e3fc22588883a2b027ebcd0d6b0abe564f0
Junwoo Park <junwoo.park@nearthlab.com>
2024-11-08 06:08:36 +0900

    generalize plot to 3d and arbitrary tag numbers

--- logs/tests/plotter_ble_quuppa.py
+++ logs/tests/plotter_ble_quuppa.py
@@ -8,66 +8,53 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('directory', type=str, help='CSV file to plot')
 args = arg_parser.parse_args()
 
+def translate_mocap_data(x, y, z):
+    return 6-x, z+5, y
+
 # Read the CSV file
 df_mocap = pd.read_csv(os.path.join(os.getcwd(), args.directory, 'rigid_body_sentinel.csv'))
 df_ble = pd.read_csv(os.path.join(os.getcwd(), args.directory, 'ble.csv'))
 
+df_mocap['X'], df_mocap['Y'], df_mocap['Z'] = translate_mocap_data(df_mocap['X'], df_mocap['Y'], df_mocap['Z'])
+    
 # Assuming the first column is for x-axis and the rest are for y-axis
 t_mocap = df_mocap['ArrivalTimeUs'] / 1e6
 t_ble = df_ble['ArrivalTimeUs'] / 1e6
 
-tag_dataframes = {}
+# Convert the location string to a list and extract x, y, z coordinates, and append them to the DataFrame
 df_ble[['x', 'y', 'z']] = df_ble['location'].apply(ast.literal_eval).tolist()
+df_ble['tag'] = df_ble['tagName'].str.extract('(\d+)$').astype(int)
 
-df_ble['tag_number'] = df_ble['tagName'].str.extract('(\d+)$').astype(int)
 # Separate data into different DataFrames based on tag number
-for tag_num in df_ble['tag_number'].unique():
-    tag_data = df_ble[df_ble['tag_number'] == tag_num]
+tag_dataframes = {}
+tags = sorted(df_ble['tag'].unique())
+for tag in tags:
+    tag_data = df_ble[df_ble['tag'] == tag]
     
-    # Create separate DataFrames for x and y coordinates
-    tag_dataframes[tag_num] = {
-        'x': tag_data[['ArrivalTimeUs', 'x']].copy(),
-        'y': tag_data[['ArrivalTimeUs', 'y']].copy()
+    # Create separate DataFrame, each tag
+    tag_dataframes[tag] = {
+        'x': tag_data[['ArrivalTimeUs', 'x']].copy().rename(columns={'x': 'value'}),
+        'y': tag_data[['ArrivalTimeUs', 'y']].copy().rename(columns={'y': 'value'}),
+        'z': tag_data[['ArrivalTimeUs', 'z']].copy().rename(columns={'z': 'value'})
     }
 
-# Convert the location string to a list and extract x, y, z coordinates
-df_ble[['x', 'y', 'z']] = df_ble['location'].apply(ast.literal_eval).tolist()
-
 # Create the plot
-fig, (ax1, ax2, ax3) = plt.subplots(3,1, figsize=(12, 8))
+coordinates = ['X', 'Y', 'Z']
+fig, axes = plt.subplots(len(tags), 1, figsize=(12, 8))
 fig.suptitle('Location vs. Time')
 
-ax1.plot(t_mocap, 6-df_mocap['X'], label='X(mocap)')
-for i in range(3):
-    tag_num = i+1
-    x_data = tag_dataframes[tag_num]['x']
-    ax1.plot(x_data['ArrivalTimeUs']/ 1e6, x_data['x'], label=f'X(ble, tag {tag_num})')
-
-
-ax2.plot(t_mocap, df_mocap['Z']+5, label='Y(mocap)')
-for i in range(3):
-    tag_num = i+1
-    y_data = tag_dataframes[tag_num]['y']
-    ax2.plot(y_data['ArrivalTimeUs']/ 1e6, y_data['y'], label=f'Y(ble, tag {tag_num})')
-
-ax3.plot(t_mocap, df_mocap['Y'], label='Height')
-
-ax1.set_ylabel('X position')
-ax2.set_ylabel('Y position')
-ax1.set_xlabel('Time (s)')
-ax2.set_xlabel('Time (s)')
-ax3.set_ylabel('Z position')
-ax3.set_xlabel('Time (s)')
-ax1.grid(True)
-ax2.grid(True)
-ax3.grid(True)
-ax1.legend()
-ax2.legend()
-ax3.legend()
-
-
-print(tag_dataframes[2]['x']['ArrivalTimeUs']/ 1e6)
-
+for i, axis in enumerate(axes):
+    # Plot the mocap data
+    axis.plot(t_mocap, df_mocap[coordinates[i]], label=f'{coordinates[i]}(mocap)')
+    # Plot the BLE data, differentiating by tag number
+    for tag in tags:
+        data = tag_dataframes[tag][coordinates[i].lower()]
+        axis.plot(data['ArrivalTimeUs']/ 1e6, data['value'], label=f'{coordinates[i]}(ble, tag {tag})')
+    # Set the title, labels, and legend
+    axis.set_title(f'{coordinates[i]} position')
+    axis.set_xlabel('Time (s)')
+    axis.grid(True)
+    axis.legend()
 
 plt.tight_layout()
 plt.show()
